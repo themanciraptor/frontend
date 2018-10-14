6afb26c9eb6a94796c14cc8d724d3cfdf0228079
MMeer <meer200m@uregina.ca>
2018-10-14 12:05:53 -0600

    git cherrypicking in order to separate the node module changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentHomepageComponent } from './homepages/student/student-homepage/student-homepage.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {StudentHomepageComponent} from './homepages/student/student-homepage/student-homepage.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'index', component: StudentHomepageComponent },
+
+    { path: '', component: LandingPageComponent, pathMatch: 'full' },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'index', component: StudentHomepageComponent },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,25 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { LoginPageModule } from './login-page/login-page.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { StudentHomepageComponent } from './homepages/student/student-homepage/student-homepage.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPageComponent,
     StudentHomepageComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     LoginPageModule,
     MatToolbarModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
