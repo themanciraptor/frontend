2c875d3addb900c281a7db23a3f638785c87873c
Hayden Blaise <haydenlueck@gmail.com>
2018-10-30 20:00:56 -0600

    linting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
-import {StudentProfileComponent} from "./student-profile/student-profile.component";
+import {StudentProfileComponent} from './student-profile/student-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
